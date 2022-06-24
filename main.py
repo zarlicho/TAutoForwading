99718e2e2712f8783ebf933eebf54ff103ccaaf7
zarlicho <63176268+zarlicho@users.noreply.github.com>
2022-06-24 18:36:00 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ from telethon import TelegramClient, events
 from telethon import TelegramClient, events, sync
 import csv
 import time
-api_id = '8977367'
-api_hash = 'd9a8cbdd0ba21647bee37edbfe322cec'
-phone = '+62085771014979'
+api_id = '19375432'
+api_hash = 'd8d30e25ef10976279733d89d601f9e4'
+phone = '+62085233025941'
 client = TelegramClient(phone, api_id, api_hash)
 
 client.connect()
@@ -16,16 +16,17 @@ if not client.is_user_authorized():
     client.send_code_request(phone)
     client.sign_in(phone, input('Enter the code: '))
 
+
+
 chats = []
 last_date = None
 chunk_size = 200
 groups=[]
 bot = []
-channel = "1623988554"
+channel = "19375432"
 # get all channels
 channel_username = 'DataSaham2Bot'# your channel
-# groupUsername = "JOSS ALERT"# your group
-groupUsername = "9A 2021"# your group
+groupUsername = "JOSS ALERT"# your group
 # message = []
 # for message in client.get_messages(channel_username, limit=1):
 #     # get last message text
